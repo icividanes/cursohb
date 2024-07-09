40818b4a5541022714647b175eb0f9046dbc31e6
Pedro Hurtado <pedrohurt@gmail.com>
2024-07-09 11:07:46 +0200

    add solid

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -2,6 +2,7 @@ package com.example;
 
 //import java.util.Set;
 import java.util.UUID;
+import java.util.function.Consumer;
 
 //import org.reflections.Reflections;
 
@@ -11,10 +12,13 @@ import org.hibernate.boot.MetadataSources;
 import org.hibernate.boot.registry.StandardServiceRegistry;
 import org.hibernate.boot.registry.StandardServiceRegistryBuilder;*/
 
+import com.example.liskov.Aguila;
+import com.example.liskov.Pinguino;
+import com.example.liskov.Writer;
 import com.example.pizza.Ingredient;
 import com.example.pizza.Pizza;
 
-import jakarta.persistence.Entity;
+//import jakarta.persistence.Entity;
 
 /**
  * Hello world!
@@ -23,65 +27,44 @@ import jakarta.persistence.Entity;
 public class App {
 
     public static void main(String[] args) {
+        Consumer<Object> mock = (obj)->{};
 
-        /*Reflections reflections = new Reflections("com.example");
-        Set<Class<?>> importantClasses = reflections.getTypesAnnotatedWith(Entity.class);
-        for (Class<?> clazz : importantClasses) {
-            
-        }*/
-        UUID id = UUID.randomUUID();
-
-        Pizza pizza = Pizza
-                .builder()
-                .setId(UUID.randomUUID())
-                .setName("carbonara")
-                .setDescription("pizza excelente")
-                .setUrl("url")
-                .build();
-
-        /*
-         * Pizza pizza = Pizza.create(UUID.randomUUID(),
-         * "carbonara",
-         * "la mejor del mundo",
-         * "url");
-         */
-        pizza.addIngredient(Ingredient.create(id, "tomate", 1.0D));
-        pizza.addIngredient(Ingredient.create(id, "tomate", 1.0D));
-        // esto no se debería permitir
-        // pizza.getIngredients().add(ingredient);
-        System.err.println(pizza.getPrice());
-
-        // EntityBase entiy = new EntityBase(UUID.randomUUID());
-        System.out.println("Hello World!");
-        // setup();
+        Pinguino pinguino = new Pinguino(5);
+        Aguila aguila = new Aguila(20, 100);
+        Writer.printAve(aguila, mock);
+        Writer.printAve(pinguino,(obj)->{});
+        //error de compilacion
+        //Writer.printAvVoladora(pinguino);
+        //Writer.printAvNoVoladora(aguila);
+       Writer.printAvNoVoladora(pinguino,System.out::println);
+       Writer.printAvVoladora(aguila,System.out::println);
     }
 
     public static void setup() {
-        /*
-         * final StandardServiceRegistry registry = new StandardServiceRegistryBuilder()
-         * .build();
-         * 
-         * SessionFactory sessionFactory = new MetadataSources(registry)
-         * .addAnnotatedClass(Events.class)
-         * .buildMetadata()
-         * .buildSessionFactory();
-         * 
-         * Session session = sessionFactory.openSession();
-         * 
-         * var tr = session.beginTransaction();
-         * 
-         * Events events = new Events();
-         * events.id = 1;
-         * 
-         * session.persist(events);
-         * // var result = session.get(Events.class, 1);
-         * // session.remove(events);
-         * 
-         * tr.commit();
-         * 
-         * // var result = session.get(Events.class, 1);
-         * 
-         * session.close();
-         */
+        
+          /*final StandardServiceRegistry registry = new StandardServiceRegistryBuilder()
+          .build();
+          
+          SessionFactory sessionFactory = new MetadataSources(registry)
+          .addAnnotatedClass(Events.class)
+          .buildMetadata()
+          .buildSessionFactory();
+          
+          Session session = sessionFactory.openSession();          
+          
+          var tr = session.beginTransaction();
+          
+          Events events = new Events();
+          events.id = 1;          
+          session.persist(events);
+          // var result = session.get(Events.class, 1);
+          // session.remove(events);
+          
+          tr.commit();
+          
+          // var result = session.get(Events.class, 1);
+          
+          session.close();*/
+         
     }
 }
